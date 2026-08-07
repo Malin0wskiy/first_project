b93583ce56ae35df0f1dd24fc875de5b2353fcf8
Malin0wskiy <malinowskiy.00@mail.ru>
2026-08-07 19:54:43 +0300

    Добавил обработку ошибок при удалении задач

--- main.py
+++ main.py
@@ -36,12 +36,18 @@ while True:
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
 
-        number = int(input("Введите номер задачи для удаления: "))
-        if number > 0 and number <= len(tasks):
-            removed_task = tasks.pop(number - 1)
-            print(f"Удалена задача: {removed_task}")
-        else:
-            print("Такой задачи нет")
+        try:
+            number = int(input("Введите номер задачи для удаления: "))
+            if number > 0 and number <= len(tasks):
+                removed_task = tasks.pop(number - 1)
+                print(f"Удалена задача: {removed_task}")
+            else:
+                print("Такой задачи нет")
+        except ValueError:
+            print("Ошибка: нужно ввести число")
+
+        except ValueError:
+            print("Ошибка: нужно ввести число")
 
     elif choice == "4":
         print("До свидания!")
